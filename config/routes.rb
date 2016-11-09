e8636fbcc62b9c335242d6c146de50dc9c48df28
Simon Lee <slee@bslogin.com>
2016-11-08 21:38:27 -0800

    fb test

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+    
+  root to: 'dashboard#index'
+  
+  resources 'dashboard'
+  
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
